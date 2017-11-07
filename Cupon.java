fb4a5b35072e89667290f1f66d6d0b937db7ada0
acevedoanalia <analiaacevedo020@gmail.com>
2017-11-07 10:05:09 -0300

    Agregue el archivo Cupon.java

---
+++ Cupon.java
@@ -0,0 +1,42 @@
+package fiuba.algo3.tp1;
+
+public class Cupon {
+	
+	int porcentajeDescuento;
+	
+	
+	public Cupon(){
+		porcentajeDescuento=0;
+		
+	}
+	
+	
+	public double calcularDescuentoDe(double precioDeLosProductos){
+		
+		double descuento=0;
+		double totalDescuento=0;
+		descuento=(double)porcentajeDescuento/100;
+		
+		totalDescuento=((precioDeLosProductos-((double)(precioDeLosProductos*descuento)))/precioDeLosProductos);
+	
+		return totalDescuento;	
+		
+	}
+	
+	public int getPorcentajeDelCupon(){
+		
+		return porcentajeDescuento;
+	}
+	
+	public void setPorcentajeDeDescuento(int unPorcentaje) throws PorcentajeNoPuedeSerNegativo{
+		
+		if(unPorcentaje>0){
+			
+		porcentajeDescuento=unPorcentaje;}
+		else {
+			throw new PorcentajeNoPuedeSerNegativo();
+		}
+		
+		
+	}
+}
